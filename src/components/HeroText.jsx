35b570933be6796090c8739eb5d6fcecf3d90463
ms-matthew <pcmaster123321@gmail.com>
2025-06-16 21:17:48 +0200

    Another commit - Hero section

---
+++ src/components/HeroText.jsx
@@ -0,0 +1,11 @@
+const HeroText = () => {
+    return (
+        <div className="z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text">
+            {/*Desktop view*/}
+            <div className="flex-col hidden md:flex c-space">
+                <h1>Hi I'm Matthew</h1>
+            </div>
+            {/*Mobile view*/}
+        </div>
+    )
+}
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+    return (
+        <section className="flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space">
+            <HeroText></HeroText>
+        </section>
+    )
+}
+
+export default Hero
